2c90b05c22239557439ba4a0b3697b1d76493d1f
noto <noto@notochihironoMacBook-Pro.local>
2019-11-11 00:41:13 +0900

    編集・更新・削除機能追加、strong parameters設定

--- app/controllers/speaks_controller.rb
+++ app/controllers/speaks_controller.rb
@@ -9,19 +9,48 @@ def new
 end
 
 def create
+  @speak = Speak.new(speak_params)
+  if params[:back]
+    render :new
+
+  else
+    if @speak.save
+      redirect_to speaks_path, notice: "投稿しました！"
+    else
+      render 'new'
+    end
+  end
 end
 
 def edit
+  @speak = Speak.find(params[:id])
 end
 
 def update
+  @speak = Speak.find(params[:id])
+  if @speak.update(speak_params)
+    redirect_to speaks_path, notice: "編集しました！"
+  else
+    render :edit
+  end
 end
 
 def destroy
+  @speak = Speak.find(params[:id])
+  @speak.destroy
+  redirect_to speaks_path, notice: "削除しました！"
 end
+
 
 def confirm
 end
+
+private
+
+def speak_params
+  params.require(:speak).permit(:content)
+end
+
 
 
 end
