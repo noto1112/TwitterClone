b47a8cc0ea2ded63ddfc2fab3ac598b75c590c7b
noto <noto@notochihironoMacBook-Pro.local>
2019-11-10 12:58:57 +0900

    controller内アクション記載、view ファイル作成

--- app/controllers/speaks_controller.rb
+++ app/controllers/speaks_controller.rb
@@ -1,2 +1,25 @@
 class SpeaksController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+def confirm
+end
+
+
 end
