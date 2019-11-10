509bd7b5c623c573385ac1761c627a6a6a302293
noto <noto@notochihironoMacBook-Pro.local>
2019-11-11 02:48:08 +0900

    タイムゾーン、CSS、コントローラー共通処理まとめ

--- app/controllers/speaks_controller.rb
+++ app/controllers/speaks_controller.rb
@@ -1,58 +1,58 @@
 class SpeaksController < ApplicationController
+  before_action :set_speak, only: [:edit, :update, :destroy]
 
-def index
-  @speaks = Speak.all
-end
+  def index
+    @speaks = Speak.all
+  end
 
-def new
-  @speak = Speak.new
-end
+  def new
+    @speak = Speak.new
+  end
 
-def create
-  @speak = Speak.new(speak_params)
-  if params[:back]
-    render :new
+  def create
+    @speak = Speak.new(speak_params)
+    if params[:back]
+      render :new
 
-  else
-    if @speak.save
-      redirect_to speaks_path, notice: "投稿しました！"
     else
-      render 'new'
+      if @speak.save
+        redirect_to speaks_path, notice: "投稿しました！"
+      else
+        render 'new'
+      end
     end
   end
-end
 
-def edit
-  @speak = Speak.find(params[:id])
-end
-
-def update
-  @speak = Speak.find(params[:id])
-  if @speak.update(speak_params)
-    redirect_to speaks_path, notice: "編集しました！"
-  else
-    render :edit
+  def edit
   end
-end
 
-def destroy
-  @speak = Speak.find(params[:id])
-  @speak.destroy
-  redirect_to speaks_path, notice: "削除しました！"
-end
+  def update
+    if @speak.update(speak_params)
+      redirect_to speaks_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
 
-
-def confirm
-  @speak = Speak.new(speak_params)
-  render :new if @speak.invalid?
-end
-
-private
-
-def speak_params
-  params.require(:speak).permit(:content)
-end
+  def destroy
+    @speak.destroy
+    redirect_to speaks_path, notice: "削除しました！"
+  end
 
 
+  def confirm
+    @speak = Speak.new(speak_params)
+    render :new if @speak.invalid?
+  end
+
+  private
+
+  def speak_params
+    params.require(:speak).permit(:content)
+  end
+
+  def set_speak
+    @speak = Speak.find(params[:id])
+  end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,9 @@ module Hitokoto
   class Application < Rails::Application
 
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
-    
+
   end
 end
